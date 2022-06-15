af1a272ff0213802a12026755a4202783717c889
nehemias10 <nehemiasrivero1@gmail.com>
2022-06-15 00:09:39 +0000

    primero

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("p").innerHTML = "hola";
 };
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+let puntoCom = ".com";
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k] + puntoCom);
+    }
+  }
+}
